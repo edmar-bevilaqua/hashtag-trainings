102b1e74a6496bb5526323c88b6fb6be5a242c94
Edmar <edmar.bevi@gmail.com>
2025-08-01 18:35:23 -0300

    feat: add first_steps/

---
+++ first_steps/hello_world.js
@@ -0,0 +1,12 @@
+//Comentário - atalho - Ctrl + ;
+//declaração do tipo + nome da vairável = informação
+
+let mensagem = "Hello World!";
+console.log(mensagem);
+
+//declaração do tipo LET
+let cor = "red";
+console.log(cor);
+
+cor = "blue";
+console.log(cor);
